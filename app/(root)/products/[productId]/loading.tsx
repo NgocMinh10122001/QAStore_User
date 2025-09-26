703d1ee1fbda85b0cc858b6a6fd768f999b95c67
Phạm Ngọc <phamngocminh10122001@gmail.com>
2025-09-27 03:50:42 +0700

    add loading

---
+++ app/(root)/products/[productId]/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <div className="w-screen h-screen flex justify-center items-center"><p>Loading...</p></div>
+}
