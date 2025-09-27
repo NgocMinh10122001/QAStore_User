6c7c6d2a6e4fd8d10d838011147d84e13101ca27
Phạm Ngọc <phamngocminh10122001@gmail.com>
2025-09-27 21:20:13 +0700

    fix loader2

--- components/custom-ui/LoaderCustom.tsx
+++ components/custom-ui/LoaderCustom.tsx
@@ -4,7 +4,7 @@ import { Loader } from "lucide-react";
 const LoaderCustom = () => {
   return (
     <div className="w-screen h-screen fixed flex justify-center items-center ">
-    <Loader color="white" size={35} className="relative z-20 animate-spin"/>
+    <Loader color="black" size={35} className="relative z-20 animate-spin"/>
     <div className="bg-grey-2 absolute top-0 left-0 w-full h-full z-10 "></div>
   </div>
   )
