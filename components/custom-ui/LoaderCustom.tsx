b390b0dc2a575653add54cdcbd427b5e30d7acae
Phạm Ngọc <phamngocminh10122001@gmail.com>
2025-09-27 21:15:05 +0700

    fix loader

--- components/custom-ui/LoaderCustom.tsx
+++ components/custom-ui/LoaderCustom.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
+import { Loader } from "lucide-react";
 
 
 const LoaderCustom = () => {
   return (
     <div className="w-screen h-screen fixed flex justify-center items-center ">
+    <Loader color="white" size={35} className="relative z-20 animate-spin"/>
     <div className="bg-grey-2 absolute top-0 left-0 w-full h-full z-10 "></div>
-    {/* <Loader color="white" size={35} className="relative z-20 animate-spin"/> */}
-    <Image src="/images/home/qastore_logo3.png" alt="logo" width={200} height={200} className="animate-ping relative z-20"/>
   </div>
   )
 }
